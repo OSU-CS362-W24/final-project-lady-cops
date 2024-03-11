79c3da1c7e4bba769310e8744b24a8f9c1c7409e
root <root@Rohans-Desktop>
2024-03-11 14:13:59 -0700

    Green: using saveChart to save data to localStorage

--- src/__tests__/u-example.js
+++ src/__tests__/u-example.js
@@ -1,4 +1,10 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const sortPoints = require('../lib/sortPoints')
+const chartStorage = require('../lib/chartStorage')
+require('@testing-library/jest-dom')
 
 test('sortPoints function is able to sort a given array', () => {
     // Arrange- creating an unsorted array to test
@@ -39,4 +45,16 @@ test('sortPoints function outputs the same array if the array is already sorted'
     expect(sortedAr[0]).toEqual({ x: 5, y: 5})
     expect(sortedAr[1]).toEqual({ x: 10, y: 10})
     expect(sortedAr[2]).toEqual({ x: 15, y: 15})
+});
+
+test('using saveChart to save data to localStorage', () => {
+    // Arrange- Creating test data to be saved to localStorage
+    const testChart = ('test data')
+    
+    // Act- Saving test data using saveChart and fetching chart data
+    chartStorage.saveChart(testChart)
+    const loadAllSavedCharts = window.localStorage.getItem("savedCharts")
+
+    // Assert- expecting the localStorage chart data to contain test data
+    expect(loadAllSavedCharts).toContain('test data')
 });
