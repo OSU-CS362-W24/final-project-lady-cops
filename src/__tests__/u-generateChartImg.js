f0fa83280066380e4e6debca390c63f34800d02c
root <root@Rohans-Desktop>
2024-03-12 11:28:49 -0700

    New file for generateChartImg

---
+++ src/__tests__/u-generateChartImg.js
@@ -0,0 +1,10 @@
+const generateChartImg = require('../lib/generateChartImg')
+require('@testing-library/jest-dom')
+
+test('using saveChart to save data to localStorage', () => {
+    // Arrange- Creating test data to be saved to localStorage
+    
+    // Act- Saving test data using saveChart and fetching chart data
+
+    // Assert- expecting the localStorage chart data to contain test data
+});
