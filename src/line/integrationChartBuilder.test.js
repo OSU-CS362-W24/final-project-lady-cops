17e991319fa45c36b13eafdcd7425cdfda9e0561
noahdude1203 <gissn@oregonstate.edu>
2024-03-16 11:42:47 -0700

    Spying

--- src/line/integrationChartBuilder.test.js
+++ src/line/integrationChartBuilder.test.js
@@ -36,28 +36,61 @@ test('Pressing "+" adds exactly one empty x and y value box', async function(){
     expect(addButton).toHaveTextContent("+")
 
     //count all cells
-    var xLabels = await domTesting.getAllByLabelText(document, "X")
-    var yLabels = await domTesting.getAllByLabelText(document, "Y")
+    var xValues = await domTesting.getAllByLabelText(document, "X")
+    var yValues = await domTesting.getAllByLabelText(document, "Y")
 
 
     // Assert that the lists are not empty
-	expect(xLabels).toHaveLength(1)
-    expect(yLabels ).toHaveLength(1)
+	expect(xValues).toHaveLength(1)
+    expect(yValues ).toHaveLength(1)
 
     const user = userEvent.setup()
 
     await user.click(addButton)
 
-    xLabels = domTesting.getAllByLabelText(document, "X")
+    xValues = domTesting.getAllByLabelText(document, "X")
     yLabels = domTesting.getAllByLabelText(document, "Y")
 
-    expect(xLabels).toHaveLength(2)
-    expect(yLabels).toHaveLength(2)
+    expect(xValues).toHaveLength(2)
+    expect(yValues).toHaveLength(2)
 })
 
 
 //ALERTS DISPLAYED FOR MISSING CHART DATA
 test('Error message appears for missing chart title', async function(){
+    initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`)
+
+    const xLabel = domTesting.getByLabelText(document, "X label")
+    const yLabel = domTesting.getByLabelText(document, "Y label")
+    
+    const xValue = domTesting.getByLabelText(document, "X")
+    const yValue = domTesting.getByLabelText(document, "Y")
+
+    const chartTitle = domTesting.getByLabelText(document, "Chart title")
+
+    const buttons = domTesting.queryAllByRole(document, "button")
+    var generateButton = buttons[2]
+
+    //make sure the correct button is selected
+    expect(generateButton).toHaveTextContent("Generate chart")
+
+    //Popups
+    const alertMock = jest.spyOn(window,'alert').mockImplementation(); 
+    const { getByText, getByTestId } = render(<Form />)
+
+    const user = userEvent.setup()
+
+    await user.type(xLabel, "X Label Name :)")
+    await user.type(yLabel, "Y Label Name :(")
+    await user.type(xValue, "22")
+    await user.type(yValue, "33")
+    await user.type(chartTitle, "") //missing chart title
+
+    await user.click(generateButton)
+    
+    expect(alertMock).toHaveBeenCalledTimes(1)
+
+
 })
 test('Error message appears for missing X Label', async function(){
 })
