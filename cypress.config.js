b8c7ef1f711c5677d8597f174fb03edb7f03f350
Nate <85893453+radiantBear@users.noreply.github.com>
2024-03-17 14:46:41 -0700

    Fix Cypress config so tests pass

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   video: true,
   e2e: {
-    setupNodeEvents(on, config) {
-      baseUrl: 'localhost:3000'
-    },
+    baseUrl: 'http://localhost:3000'
   }
 });
