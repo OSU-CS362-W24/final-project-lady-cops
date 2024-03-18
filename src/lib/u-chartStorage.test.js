7bcf93917951028dda79e6b407fe3865a6c848e3
Rohan Thapliyal <thapliyr@oregonstate.edu>
2024-03-18 16:14:07 -0700

    Moving unit tests

---
+++ src/lib/u-chartStorage.test.js
@@ -0,0 +1,113 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const fs = require("fs")
+const chartStorage = require('./chartStorage')
+require('@testing-library/jest-dom')
+
+function initDomFromFiles(htmlPath, jsPath) {
+    const html = fs.readFileSync(htmlPath, 'utf8')
+    document.open()
+    document.write(html)
+    document.close()
+    jest.isolateModules(function() {
+        require(jsPath)
+    })
+}
+
+test('using saveChart to save data to localStorage', () => {
+    // Arrange- Creating test data to be saved to localStorage
+    initDomFromFiles(`${__dirname}/../index.html`, `${__dirname}/../lib/chartStorage.js`)
+    const testChart = ('test data')
+    
+    // Act- Saving test data using saveChart and fetching chart data
+    chartStorage.saveChart(testChart)
+    const loadAllSavedCharts = window.localStorage.getItem("savedCharts")
+
+    // Assert- expecting the localStorage chart data to contain test data
+    expect(loadAllSavedCharts).toBe('["test data"]')
+    window.localStorage.clear()
+});
+
+test('using loadAllSavedCharts to retrieve data from localStorage', () => {
+    // Arrange- Creating test data to be saved to localStorage
+    initDomFromFiles(`${__dirname}/../index.html`, `${__dirname}/../lib/chartStorage.js`)
+    const testChart1 = ('banana')
+    const testChart2 = ('apple')
+    const testChart3 = ('orange')
+    
+    // Act- Saving test data using saveChart and fetching chart data using loadAllSavedCharts
+    chartStorage.saveChart(testChart1)
+    chartStorage.saveChart(testChart2)
+    chartStorage.saveChart(testChart3)
+    const loadAllSavedCharts = chartStorage.loadAllSavedCharts()
+
+    // Assert- expecting the call to loadAllSavedCharts to return data that contains test data
+    expect(loadAllSavedCharts).toContain('banana')
+    expect(loadAllSavedCharts).toContain('apple')
+    expect(loadAllSavedCharts).toContain('orange')
+    window.localStorage.clear()
+});
+
+test('using loadSavedChart to retrieve data from a specific index in localStorage', () => {
+    // Arrange- Creating test data to be saved to localStorage
+    initDomFromFiles(`${__dirname}/../index.html`, `${__dirname}/../lib/chartStorage.js`)
+    const testChart1 = ('banana')
+    const testChart2 = ('apple')
+    const testChart3 = ('orange')
+    
+    // Act- Saving test data using saveChart and fetching chart data using loadAllSavedCharts
+    chartStorage.saveChart(testChart1)
+    chartStorage.saveChart(testChart2)
+    chartStorage.saveChart(testChart3)
+
+    const loadSavedChart1 = chartStorage.loadSavedChart(0)
+    const loadSavedChart2 = chartStorage.loadSavedChart(1)
+    const loadSavedChart3 = chartStorage.loadSavedChart(2)
+
+    // Assert- expecting the call to loadAllSavedCharts to return data that contains test data
+    expect(loadSavedChart1).toBe('banana')
+    expect(loadSavedChart2).toBe('apple')
+    expect(loadSavedChart3).toBe('orange')
+    window.localStorage.clear()
+});
+
+test("using updateCurrentChartData to write to the current chart's data from localStorage", () => {
+    // Arrange- Creating test data to be saved to localStorage
+    initDomFromFiles(`${__dirname}/../index.html`, `${__dirname}/../lib/chartStorage.js`)
+    const testChart = ("banana")
+    
+    // Act- Saving test data using updateCurrentChartData and fetching chart data from localStorage
+    chartStorage.updateCurrentChartData(testChart)
+    const currentChartData1 = window.localStorage.getItem('currentChartData')
+
+    // Assert- expecting localStorage to the test data from currentChartData
+    expect(currentChartData1).toBe('"banana"')
+
+    // Arrange- Creating test data to be saved to localStorage
+    const testReplace = ('apple')
+    
+    // Act- Saving test data using updateCurrentChartData and fetching chart data from localStorage
+    chartStorage.updateCurrentChartData(testReplace)
+    const currentChartData2 = window.localStorage.getItem('currentChartData')
+
+    // Assert- expecting localStorage to the test data from currentChartData and not to contain previous data
+    expect(currentChartData2).not.toContain('"banana"')
+    expect(currentChartData2).toBe('"apple"')
+    window.localStorage.clear()
+});
+
+test("using loadCurrentChartData to read the current chart's data from localStorage", () => {
+    // Arrange- Creating test data to be saved to localStorage
+    initDomFromFiles(`${__dirname}/../index.html`, `${__dirname}/../lib/chartStorage.js`)
+    const testChart1 = ('apple')
+    
+    // Act- Saving test data using updateCurrentChartData and fetching chart data using loadCurrentChartData
+    chartStorage.updateCurrentChartData(testChart1)
+    const currentChartData = chartStorage.loadCurrentChartData()
+
+    // Assert- expecting localStorage to the test data from currentChartData
+    expect(currentChartData).toBe('apple')
+    window.localStorage.clear()
+});
