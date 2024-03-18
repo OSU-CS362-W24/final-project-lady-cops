e0d617ea65515ca1ff24f6e58ac9a00f65643758
Nate <85893453+radiantBear@users.noreply.github.com>
2024-03-18 00:37:54 -0700

    Update Cypress Testing Library import to Common JS syntax

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,4 +24,4 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import '@testing-library/cypress/add-commands';
+require('@testing-library/cypress/add-commands');
